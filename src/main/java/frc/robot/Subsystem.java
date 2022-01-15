8d64f82121abcf041fb67a25cc0a037a468417dd
Emory George <epgeorge@laca.org>
2022-01-14 19:41:04 -0500

    Initial Commit
    initial commit

---
+++ src/main/java/frc/robot/Subsystem.java
@@ -0,0 +1,40 @@
+package frc.robot;
+
+//Imports
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+//Subsystem class is general - specific methods for each subsystem
+public class Subsystem {
+    
+    //Wheel Spin Variables
+    private Spark wheelSpin = new Spark(0); //need pwm channel
+    private DoubleSolenoid wheelSpinLift = new DoubleSolenoid(PneumaticsModuleType.CTREPCM,/*the forward pcm id*/1, /*the reverse pcm id*/2);
+
+    //Constructor will be called in Robot.java to create Subsystem object
+    public Subsystem(){}
+
+    //Wheel Spin Method
+    //Assign buttons to wheelLift, wheelLower, spinRight, and spinLeft
+    public void wheelSpin(boolean wheelLift, boolean wheelLower, boolean spinRight, boolean spinLeft){
+        //Controls pneumatic lifting mechanism
+        //May need to nest the spinning control inside the if statement for wheelLift
+        if(wheelLift){
+            wheelSpinLift.set(Value.kForward);
+        }
+        else if(wheelLower){
+            wheelSpinLift.set(Value.kReverse);
+        }
+        //Controls motor that spins wheel
+        if (spinRight){
+            wheelSpin.set(0.5);
+        }
+        else if (spinLeft){
+            wheelSpin.set(-0.5);
+        }
+    }
+
+
+}
